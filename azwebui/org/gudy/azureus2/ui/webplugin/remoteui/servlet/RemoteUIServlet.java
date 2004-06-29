d67ab13d04246b4cbbfa65dce4385e1d2f2ac4d6
parg <parg@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2004-06-29 21:41:17 +0000

    *** empty log message ***
    git-svn-id: http://svn.vuze.com/public/plugins@20833 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- azwebui/org/gudy/azureus2/ui/webplugin/remoteui/servlet/RemoteUIServlet.java
+++ azwebui/org/gudy/azureus2/ui/webplugin/remoteui/servlet/RemoteUIServlet.java
@@ -85,6 +85,8 @@ RemoteUIServlet
 		"ui/webplugin/remoteui/applet/RemoteUIApplet$2.class",
 		"ui/webplugin/remoteui/applet/RemoteUIApplet$3.class",
 		"ui/webplugin/remoteui/applet/RemoteUIApplet$4.class",
+		"ui/webplugin/remoteui/applet/RemoteUIApplet$5.class",
+		"ui/webplugin/remoteui/applet/RemoteUIApplet$6.class",
 		"ui/webplugin/remoteui/applet/RemoteUIMainPanelAdaptor.class",
 		"ui/webplugin/remoteui/applet/RemoteUIMainPanel.class",
 		"ui/webplugin/remoteui/applet/model/MDDownloadModel.class",
