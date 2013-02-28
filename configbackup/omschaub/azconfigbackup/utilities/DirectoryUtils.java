6ddcddb84b594148259a9b9f35d5bfa26d257e51
pgardner <pgardner@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2013-02-28 22:38:04 +0000

    Fix install directory for OSX Java 7
    git-svn-id: http://svn.vuze.com/public/plugins@28080 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- configbackup/omschaub/azconfigbackup/utilities/DirectoryUtils.java
+++ configbackup/omschaub/azconfigbackup/utilities/DirectoryUtils.java
@@ -31,7 +31,7 @@ public class DirectoryUtils {
 	};
 
     public static String getBackupDirectory(){
-    String installDirectory = new String (System.getProperty("user.dir"));
+	String installDirectory = View.getPluginInterface().getUtilities().getAzureusProgramDir();
     installDirectory = doubleSlash_At_End_Check(installDirectory);
     String backupInstallDirectory = View.getPluginInterface().getPluginconfig().getPluginStringParameter("backup_directory", installDirectory + System.getProperty("file.separator") + "backup");
     
