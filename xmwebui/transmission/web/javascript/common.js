8277b71c0bc225d775b5d5a0a77b6389bd041dd8
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2013-09-23 17:16:08 +0000

    add jQuery hasParent implementation from http://stackoverflow.com/a/2389549
    git-svn-id: http://svn.vuze.com/public/plugins@29053 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- xmwebui/transmission/web/javascript/common.js
+++ xmwebui/transmission/web/javascript/common.js
@@ -24,6 +24,18 @@ if (!Array.indexOf){
 	}
 }
 
+// hasParent implementation from http://stackoverflow.com/a/2389549
+jQuery.extend( jQuery.fn, {
+    // Name of our method & one argument (the parent selector)
+    hasParent: function( p ) {
+        // Returns a subset of items using jQuery.filter
+        return this.filter(function () {
+            // Return truthy/falsey based on presence in parent
+            return $(p).find(this).length;
+        });
+    }
+});
+
 //http://forum.jquery.com/topic/combining-ui-dialog-and-tabs
 $.fn.tabbedDialog = function (dialog_opts) {
 	this.tabs({selected: 0});
