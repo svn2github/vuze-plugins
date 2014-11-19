31b4fa8f09ca5cc9dc014b6951c26b9ae41686a1
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2014-11-19 18:14:49 +0000

    promoplugin
    git-svn-id: http://svn.vuze.com/public/plugins@41096 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

---
+++ azpromo/com/vuze/azureus/plugin/azpromo/PromoPlugin.java
@@ -0,0 +1,209 @@
+/**
+ * Created on Sep 29, 2014
+ *
+ * Copyright Azureus Software, Inc.  All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA 
+ */
+
+package com.vuze.azureus.plugin.azpromo;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.gudy.azureus2.core3.internat.MessageText;
+import org.gudy.azureus2.platform.PlatformManager;
+import org.gudy.azureus2.platform.PlatformManagerFactory;
+import org.gudy.azureus2.plugins.*;
+import org.gudy.azureus2.plugins.config.ConfigParameter;
+import org.gudy.azureus2.plugins.config.ConfigParameterListener;
+import org.gudy.azureus2.plugins.ui.UIInstance;
+import org.gudy.azureus2.plugins.ui.UIManager;
+import org.gudy.azureus2.plugins.ui.UIManagerListener;
+import org.gudy.azureus2.plugins.ui.config.BooleanParameter;
+import org.gudy.azureus2.plugins.ui.model.BasicPluginConfigModel;
+import org.gudy.azureus2.plugins.utils.FeatureManager;
+import org.gudy.azureus2.plugins.utils.FeatureManager.Licence;
+import org.gudy.azureus2.ui.swt.plugins.UISWTInstance;
+
+import com.aelitis.azureus.core.messenger.PlatformMessage;
+import com.aelitis.azureus.core.messenger.PlatformMessenger;
+import com.aelitis.azureus.util.FeatureUtils;
+
+/**
+ * @created Sep 29, 2014
+ */
+public class PromoPlugin
+	implements UnloadablePlugin
+{
+
+	private static final String VIEWID = "SidebarPromo";
+
+	private UISWTInstance swtInstance = null;
+
+	public static PluginInterface pluginInterface;
+
+	public static String readStringFromUrl(String url) {
+		StringBuffer sb = new StringBuffer();
+		try {
+			URL _url = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) _url.openConnection();
+
+			InputStream is = con.getInputStream();
+
+			byte[] buffer = new byte[256];
+
+			int read = 0;
+
+			while ((read = is.read(buffer)) != -1) {
+				sb.append(new String(buffer, 0, read));
+			}
+			con.disconnect();
+
+		} catch (Throwable e) {
+
+		}
+		return sb.toString();
+	}
+
+	// @see org.gudy.azureus2.plugins.Plugin#initialize(org.gudy.azureus2.plugins.PluginInterface)
+	public void initialize(final PluginInterface pi) throws PluginException {
+		this.pluginInterface = pi;
+
+// We are usually initialized before FeatMan, so this check isn't very usefull
+//		if (pi.getUtilities().getFeatureManager() != null) {
+//  		boolean hasFullLicence = FeatureUtils.hasFullLicence();
+//  		if (hasFullLicence) {
+//  			return;
+//  		}
+//		}
+
+		UIManager uiManager = pluginInterface.getUIManager();
+		BasicPluginConfigModel configModel = uiManager.createBasicPluginConfigModel("ConfigView.Section."
+				+ VIEWID);
+		BooleanParameter paramEnabled = configModel.addBooleanParameter2("enabled",
+				VIEWID + ".enabled", true);
+		paramEnabled.addConfigParameterListener(new ConfigParameterListener() {
+			public void configParameterChanged(ConfigParameter param) {
+				UIInstance[] uiInstances = pluginInterface.getUIManager().getUIInstances();
+				for (UIInstance uiInstance : uiInstances) {
+					if (uiInstance instanceof UISWTInstance) {
+						swtInstance = (UISWTInstance) uiInstance;
+						break;
+					}
+				}
+				if (swtInstance != null) {
+					boolean enabled = pluginInterface.getPluginconfig().getPluginBooleanParameter(
+							"enabled");
+					if (enabled) {
+						swtInstance.addView(UISWTInstance.VIEW_SIDEBAR_AREA, VIEWID,
+								PromoView.class, null);
+					} else {
+						swtInstance.removeViews(UISWTInstance.VIEW_SIDEBAR_AREA, VIEWID);
+
+						PromoPlugin.logEvent("goaway");
+					}
+				}
+			}
+		});
+
+		boolean enabled = pluginInterface.getPluginconfig().getPluginBooleanParameter(
+				"enabled");
+		if (enabled) {
+
+			// Get notified when the UI is attached
+			pluginInterface.getUIManager().addUIListener(new UIManagerListener() {
+				public void UIAttached(UIInstance instance) {
+					if (instance instanceof UISWTInstance) {
+						swtInstance = ((UISWTInstance) instance);
+						swtInstance.addView(UISWTInstance.VIEW_SIDEBAR_AREA, VIEWID,
+								PromoView.class, null);
+					}
+				}
+
+				public void UIDetached(UIInstance instance) {
+					swtInstance = null;
+				}
+			});
+
+			pluginInterface.addListener(new PluginListener() {
+				public void initializationComplete() {
+					if (pluginInterface == null) {
+						return;
+					}
+
+					FeatureManager fm = pluginInterface.getUtilities().getFeatureManager();
+
+					FeatureManager.FeatureManagerListener fml = new FeatureManager.FeatureManagerListener() {
+
+						public void licenceRemoved(Licence licence) {
+							checkLicence();
+						}
+
+						public void licenceChanged(Licence licence) {
+							checkLicence();
+						}
+
+						public void licenceAdded(Licence licence) {
+							checkLicence();
+						}
+					};
+
+					fm.addListener(fml);
+
+					checkLicence();
+				}
+
+				public void closedownInitiated() {
+				}
+
+				public void closedownComplete() {
+				}
+			});
+
+		}
+
+	}
+
+	protected void checkLicence() {
+		boolean hasFullLicence = FeatureUtils.hasFullLicence();
+
+		if (hasFullLicence) {
+			pluginInterface.getPluginconfig().setPluginParameter("enabled", false);
+		}
+	}
+
+	// @see org.gudy.azureus2.plugins.UnloadablePlugin#unload()
+	public void unload() throws PluginException {
+		if (swtInstance != null)
+			swtInstance.removeViews(UISWTInstance.VIEW_SIDEBAR_AREA, VIEWID);
+	}
+
+	public static void logEvent(String event) {
+		PlatformManager pm = PlatformManagerFactory.getPlatformManager();
+		if (pm == null) {
+			return;
+		}
+		Object[] params = new Object[] {
+			"locale",
+			MessageText.getCurrentLocale().toString(),
+			"event-type",
+			event
+		};
+		PlatformMessage message = new PlatformMessage("AZMSG", "PromoPlugin",
+				"log", params, 1000);
+		PlatformMessenger.pushMessageNow(message, null);
+	}
+
+}
---
+++ azpromo/com/vuze/azureus/plugin/azpromo/PromoView.java
@@ -0,0 +1,220 @@
+/**
+ * Created on Sep 29, 2014
+ *
+ * Copyright Azureus Software, Inc.  All rights reserved.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 2 of the License only.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA 
+ */
+
+package com.vuze.azureus.plugin.azpromo;
+
+import java.util.Locale;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+
+import org.gudy.azureus2.plugins.PluginConfig;
+import org.gudy.azureus2.ui.swt.Utils;
+import org.gudy.azureus2.ui.swt.plugins.UISWTView;
+import org.gudy.azureus2.ui.swt.plugins.UISWTViewEvent;
+import org.gudy.azureus2.ui.swt.plugins.UISWTViewEventListener;
+
+import com.aelitis.azureus.ui.swt.skin.SWTSkinObjectSash;
+import com.aelitis.azureus.ui.swt.views.skin.sidebar.SideBar;
+import com.appadx.adcontrol.*;
+
+/**
+ * @created Sep 29, 2014
+ *
+ */
+public class PromoView
+	implements UISWTViewEventListener
+{
+	private AdControlSWT adControl;
+
+	public PromoView() {
+	}
+
+	// @see org.gudy.azureus2.ui.swt.plugins.UISWTViewEventListener#eventOccurred(org.gudy.azureus2.ui.swt.plugins.UISWTViewEvent)
+	public boolean eventOccurred(UISWTViewEvent event) {
+		switch (event.getType()) {
+			case UISWTViewEvent.TYPE_CREATE:
+				log("TYPE_CREATE Called");
+				break;
+
+			case UISWTViewEvent.TYPE_INITIALIZE:
+				log("TYPE_INITIALIZE Called");
+				initialize((Composite) event.getData(), event.getView());
+				break;
+
+			case UISWTViewEvent.TYPE_REFRESH:
+				refresh(event.getView());
+				break;
+
+			case UISWTViewEvent.TYPE_DESTROY:
+				log("TYPE_DESTROY Called");
+				break;
+
+			case UISWTViewEvent.TYPE_DATASOURCE_CHANGED:
+				log("TYPE_DATASOURCE_CHANGED Called");
+				break;
+
+			case UISWTViewEvent.TYPE_FOCUSGAINED:
+				log("TYPE_FOCUSGAINED Called");
+				break;
+
+			case UISWTViewEvent.TYPE_FOCUSLOST:
+				log("TYPE_FOCUSLOST Called");
+				break;
+
+			case UISWTViewEvent.TYPE_LANGUAGEUPDATE:
+				log("TYPE_LANGUAGEUPDATE Called "
+						+ Locale.getDefault().toString());
+				break;
+		}
+		return true;
+	}
+
+	private void initialize(Composite parent, final UISWTView view) {
+
+		try {
+			PluginConfig config = PromoPlugin.pluginInterface.getPluginconfig();
+			if (!config.getPluginBooleanParameter("resized.once")) {
+				config.setPluginParameter("resized.once", true);
+				boolean visible = SideBar.instance.isVisible();
+				if (visible) {
+					final SWTSkinObjectSash soSash = (SWTSkinObjectSash) SideBar.instance.getSkin().getSkinObject(
+							"sidebar-sash");
+					if (soSash != null && soSash.getAboveSize() < 300) {
+						soSash.setAboveSize(300);
+					}
+				}
+			}
+		} catch (Throwable t) {
+		}
+
+		final Composite ourParent = parent;
+
+		ourParent.setVisible(false);
+
+		FormData fd;
+		fd = Utils.getFilledFormData();
+		fd.bottom = null;
+		fd.left = null;
+		fd.right.offset = -3;
+
+		final Label lbl = new Label(ourParent, SWT.NONE);
+		lbl.setText("x");
+		lbl.setCursor(lbl.getDisplay().getSystemCursor(SWT.CURSOR_HAND));
+		lbl.setLayoutData(fd);
+		lbl.addMouseListener(new MouseListener() {
+
+			public void mouseUp(MouseEvent e) {
+				view.closeView();
+				PromoPlugin.pluginInterface.getPluginconfig().setPluginParameter(
+						"enabled", false);
+			}
+
+			public void mouseDown(MouseEvent e) {
+			}
+
+			public void mouseDoubleClick(MouseEvent e) {
+			}
+		});
+
+		adControl = new AdControlSWT(ourParent, SWT.NO_SCROLL);
+		fd = Utils.getFilledFormData();
+		fd.height = 254;
+		fd.top = new FormAttachment(lbl, 2);
+		adControl.setLayoutData(fd);
+		adControl.addAdControlEventListener(new AdControlEventListener() {
+
+			public void onAdClicked() {
+				log("clicked");
+				super.onAdClicked();
+			}
+
+			public void onAdCompleted() {
+				log("complete");
+				super.onAdCompleted();
+			}
+
+			public void onAdError(String arg0, int arg1, int arg2) {
+				log("" + arg1 + "/" + arg2 + ": " + arg0);
+				super.onAdError(arg0, arg1, arg2);
+			}
+
+			public void onAdLogMessage(String mesg) {
+				log(mesg);
+				super.onAdLogMessage(mesg);
+			}
+
+			public void onAdStarted() {
+				log("started");
+				super.onAdStarted();
+			}
+		});
+		IAdControlOptions options = adControl.getOptions();
+		options.setPlayerOption(IAdControlOptions.Player.AUTO_MUTE, true);
+		options.setPubID("mawra2ag1");
+		options.setPageName("vuze");
+		options.setPubConfigURL("http://vuze-pubcfg.desktopadx.com/service/pubcfg/get.php?id=");   
+		options.setRequestDomain("btpr.vuze.com");
+
+		PromoPlugin.pluginInterface.getUtilities().createThread("LoadPromo",
+				new Runnable() {
+
+					public void run() {
+						try {
+							log("load");
+							adControl.loadAd();
+							Utils.execSWTThread(new Runnable() {
+								public void run() {
+									log("show");
+									if (ourParent.isDisposed()) {
+										return;
+									}
+									ourParent.setVisible(true);
+								}
+							});
+
+
+							PromoPlugin.logEvent("shown");
+						} catch (AdControlException e1) {
+							//  Bad or missing Publisher Configuration - Connection timed out: connect
+							e1.printStackTrace();
+							Utils.execSWTThread(new Runnable() {
+								public void run() {
+									log("closeErr");
+									view.closeView();
+								}
+							});
+						}
+					}
+				});
+		adControl.getShell().layout(true, true);
+	}
+
+	protected void log(String string) {
+		System.out.println("PromoView: " + string);
+	}
+
+	private void refresh(final UISWTView view) {
+	}
+
+}
