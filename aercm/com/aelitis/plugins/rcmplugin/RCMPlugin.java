db8b7a07e353e9002cbc81a0cb979a46ecf4fa1a
amogge <amogge@c2c1b7af-1477-405b-86ee-8aefd1fc4dd2>
2016-08-08 21:22:52 +0000

    web ui rpcs: 						methods.add( "rcm-lookup-start" ); 						methods.add( "rcm-lookup-remove" ); 						methods.add( "rcm-lookup-get-results" );
    git-svn-id: http://svn.vuze.com/public/plugins@43498 c2c1b7af-1477-405b-86ee-8aefd1fc4dd2

--- aercm/com/aelitis/plugins/rcmplugin/RCMPlugin.java
+++ aercm/com/aelitis/plugins/rcmplugin/RCMPlugin.java
@@ -31,16 +31,15 @@ import org.gudy.azureus2.plugins.download.Download;
 import org.gudy.azureus2.plugins.ipc.IPCException;
 import org.gudy.azureus2.plugins.ui.UIInstance;
 import org.gudy.azureus2.plugins.ui.UIManagerListener;
-import org.gudy.azureus2.plugins.utils.Utilities;
 import org.gudy.azureus2.plugins.utils.LocaleUtilities;
-import org.gudy.azureus2.plugins.utils.search.SearchProvider;
+import org.gudy.azureus2.plugins.utils.Utilities;
+import org.gudy.azureus2.plugins.utils.search.*;
 
 import com.aelitis.azureus.core.cnetwork.ContentNetwork;
-import com.aelitis.azureus.core.content.ContentException;
-import com.aelitis.azureus.core.content.RelatedContent;
-import com.aelitis.azureus.core.content.RelatedContentManager;
+import com.aelitis.azureus.core.content.*;
 import com.aelitis.azureus.ui.UserPrompterResultListener;
 import com.aelitis.azureus.util.MapUtils;
+import com.aelitis.plugins.rcmplugin.RelatedContentUISWT.SearchRelatedContent;
 
 
 public class 
@@ -86,6 +85,9 @@ RCMPlugin
 	private byte[]						source_vhdn = compressDomain( "vhdn.vuze.com" );
 	
 	private Object json_rpc_server;	// Object during migration
+	
+	private Map<String, SearchInstance> mapSearchInstances = new HashMap<String, SearchInstance>();
+	private Map<String, Map> mapSearchResults = new HashMap<String, Map>();
 	
 	private byte[]
 	compressDomain(
@@ -188,6 +190,9 @@ RCMPlugin
 					{
 						methods.add( "rcm-is-enabled" );
 						methods.add( "rcm-get-list" );
+						methods.add( "rcm-lookup-start" );
+						methods.add( "rcm-lookup-remove" );
+						methods.add( "rcm-lookup-get-results" );
 						methods.add( "rcm-set-enabled" );
 					}
 					
@@ -250,6 +255,30 @@ RCMPlugin
 							} else {
 								setToDefaultSourcesList();
 							}
+
+						} else if ( method.equals( "rcm-lookup-start" )){
+
+							if (isRCMEnabled() && isUIEnabled()) {
+								rpcLookupStart(result, args);
+							} else {
+								throw( new PluginException( "RCM not enabled" ));
+							}
+							
+						} else if ( method.equals( "rcm-lookup-remove" )){
+
+							if (isRCMEnabled() && isUIEnabled()) {
+								rpcLookupRemove(result, args);
+							} else {
+								throw( new PluginException( "RCM not enabled" ));
+							}
+							
+						} else if ( method.equals( "rcm-lookup-get-results" )){
+
+							if (isRCMEnabled() && isUIEnabled()) {
+								rpcLookupGetResults(result, args);
+							} else {
+								throw( new PluginException( "RCM not enabled" ));
+							}
 							
 						}else{
 							
@@ -263,9 +292,190 @@ RCMPlugin
 			plugin_interface.getUtilities().registerJSONRPCServer((Utilities.JSONServer)json_rpc_server);
 		}
 	}
-	
-	protected void rpcGetList(Map result, Map args) {
-		long since = args == null ? 0 : MapUtils.getMapLong(args, "since", 0);
+
+	protected void rpcLookupStart(Map result, Map args) throws PluginException {
+		String searchTerm = MapUtils.getMapString(args, "search-term", null);
+		String lookupByTorrent = MapUtils.getMapString(args, "torrent-hash", null);
+		long lookupBySize = MapUtils.getMapLong(args, "file-size", 0);
+		
+		String[] networks = new String[] {
+			AENetworkClassifier.AT_PUBLIC
+		};
+		String net_str = getNetworkString(networks);
+		try {
+			RelatedContentManager manager = RelatedContentManager.getSingleton();
+
+			if (searchTerm != null) {
+				final String lookupID = Integer.toHexString((searchTerm + net_str).hashCode());
+
+				result.put("lid", lookupID);
+
+				//SearchInstance searchInstance = mapSearchInstances.get(searchID);
+
+				Map<String, Object> parameters = new HashMap<String, Object>();
+				parameters.put(SearchProvider.SP_SEARCH_TERM, searchTerm);
+
+				//if ( networks != null && networks.length > 0 ){
+				//parameters.put( SearchProvider.SP_NETWORKS, networks );
+				//}
+
+				Map map = mapSearchResults.get(lookupID);
+				if (map == null) {
+					map = new HashMap();
+					mapSearchResults.put(lookupID, map);
+				}
+				int activeSearches = MapUtils.getMapInt(map, "active-searches", 0);
+				map.put("active-searches", ++activeSearches);
+				map.put("complete", activeSearches > 0 ? false : true);
+				SearchInstance searchRCM = manager.searchRCM(
+						parameters, new SearchObserver() {
+							
+							public void resultReceived(SearchInstance search, SearchResult result) {
+								synchronized (mapSearchResults) {
+  								Map map = mapSearchResults.get(lookupID);
+  								if (map == null) {
+  									return;
+  								}
+  								
+  								List list = MapUtils.getMapList(map, "results", null);
+  								if (list == null) {
+  									list = new ArrayList<>();
+  									map.put("results", list);
+  								}
+
+  								
+  								SearchRelatedContent src = new SearchRelatedContent( result );
+  								
+  								Map mapResult = relatedContentToMap(src);
+  								
+  								list.add(mapResult);
+
+								}
+							}
+							
+							public Object getProperty(int property) {
+								// TODO Auto-generated method stub
+								return null;
+							}
+							
+							public void complete() {
+								synchronized (mapSearchResults) {
+  								Map map = mapSearchResults.get(lookupID);
+  								if (map == null) {
+  									return;
+  								}
+  								int activeSearches = MapUtils.getMapInt(map, "active-searches", 0);
+  								if (activeSearches > 0) {
+  									activeSearches--;
+  								}
+  								map.put("active-searches", activeSearches);
+  								map.put("complete", activeSearches > 0 ? false : true);
+								}
+							}
+							
+							public void cancelled() {
+								complete();
+							}
+						});
+			} else if (lookupByTorrent != null || lookupBySize > 0) {
+
+				final String lookupID = lookupByTorrent != null ? lookupByTorrent
+						: Integer.toHexString(
+								(String.valueOf(lookupBySize) + net_str).hashCode());
+
+				result.put("lid", lookupID);
+
+				Map map = mapSearchResults.get(lookupID);
+				if (map == null) {
+					map = new HashMap();
+					mapSearchResults.put(lookupID, map);
+				}
+				int activeSearches = MapUtils.getMapInt(map, "active-searches", 0);
+				map.put("active-searches", ++activeSearches);
+				map.put("complete", activeSearches > 0 ? false : true);
+				RelatedContentLookupListener l = new RelatedContentLookupListener() {
+
+					public void lookupStart() {
+					}
+
+					public void lookupFailed(ContentException error) {
+						lookupComplete();
+					}
+
+					public void lookupComplete() {
+						synchronized (mapSearchResults) {
+							Map map = mapSearchResults.get(lookupID);
+							if (map == null) {
+								return;
+							}
+							int activeSearches = MapUtils.getMapInt(map, "active-searches",
+									0);
+							if (activeSearches > 0) {
+								activeSearches--;
+							}
+							map.put("active-searches", activeSearches);
+							map.put("complete", activeSearches > 0 ? false : true);
+						}
+					}
+
+					public void contentFound(RelatedContent[] content) {
+						synchronized (mapSearchResults) {
+							Map map = mapSearchResults.get(lookupID);
+							if (map == null) {
+								return;
+							}
+
+							List list = MapUtils.getMapList(map, "results", null);
+							if (list == null) {
+								list = new ArrayList<>();
+								map.put("results", list);
+							}
+
+							for (RelatedContent item : content) {
+								Map<String, Object> mapResult = relatedContentToMap(item);
+								list.add(mapResult);
+							}
+
+						}
+					}
+				};
+				if (lookupByTorrent != null) {
+					byte[] hash = ByteFormatter.decodeString(lookupByTorrent);
+					manager.lookupContent(hash, networks, l);
+				} else if (lookupBySize > 0) {
+					manager.lookupContent(lookupBySize, l);
+				}
+
+			} else {
+				throw new PluginException("No search-term, torrent-hash or file-size");
+			}
+		} catch (Exception e) {
+			throw new PluginException(e);
+		}
+	}
+
+	protected void rpcLookupRemove(Map result, Map args) throws PluginException {
+		String lid = MapUtils.getMapString(args, "lid", null);
+		if (lid == null) {
+			throw new PluginException("No Lookup ID");
+		}
+		mapSearchInstances.remove(lid);
+		mapSearchResults.remove(lid);
+	}
+
+	protected void rpcLookupGetResults(Map result, Map args) throws PluginException {
+		// TODO: filter by "since"
+		long since = MapUtils.getMapLong(args, "since", 0);
+
+		Map map = mapSearchResults.get(MapUtils.getMapString(args, "lid", null));
+		if (map == null) {
+			throw new PluginException("No results for Lookup ID");
+		}
+		result.putAll(map);
+	}
+
+	protected void rpcGetList(Map result, Map args) throws PluginException {
+		long since = MapUtils.getMapLong(args, "since", 0);
 		long until = 0;
 
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
@@ -278,8 +488,7 @@ RCMPlugin
 				if (!isVisible(item)) {
 					continue;
 				}
-				HashMap<String, Object> map = new HashMap<String, Object>();
-				
+
 				long changedLocallyOn = item.getChangedLocallyOn();
 				if (changedLocallyOn < since) {
 					continue;
@@ -288,27 +497,38 @@ RCMPlugin
 					until = changedLocallyOn;
 				}
 
-				map.put("changedOn", changedLocallyOn);
-				map.put("contentNetwork", item.getContentNetwork());
-				map.put("hash", ByteFormatter.encodeString(item.getHash()));
-				map.put("lastSeenSecs", item.getLastSeenSecs());
-				map.put("peers", item.getLeechers());
-				map.put("level", item.getLevel());
-				map.put("publishDate", item.getPublishDate());
-				map.put("rank", item.getRank());
-				map.put("relatedToHash", ByteFormatter.encodeString(item.getRelatedToHash()) );
-				map.put("seeds", item.getSeeds());
-				map.put("size", item.getSize());
-				map.put("tags", item.getTags());
-				map.put("title", item.getTitle());
-				map.put("tracker", item.getTracker());
-				map.put("unread", item.isUnread());
+				Map map = relatedContentToMap(item);
 				list.add(map);
 			}
-		} catch (ContentException e) {
+		} catch (Exception e) {
+			throw new PluginException(e);
 		}
 		
 		result.put("until", until);
+	}
+
+
+	private Map<String, Object> relatedContentToMap(RelatedContent item) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		
+		long changedLocallyOn = item.getChangedLocallyOn();
+
+		map.put("changedOn", changedLocallyOn);
+		map.put("contentNetwork", item.getContentNetwork());
+		map.put("hash", ByteFormatter.encodeString(item.getHash()));
+		map.put("lastSeenSecs", item.getLastSeenSecs());
+		map.put("peers", item.getLeechers());
+		map.put("level", item.getLevel());
+		map.put("publishDate", item.getPublishDate());
+		map.put("rank", item.getRank());
+		map.put("relatedToHash", ByteFormatter.encodeString(item.getRelatedToHash()) );
+		map.put("seeds", item.getSeeds());
+		map.put("size", item.getSize());
+		map.put("tags", item.getTags());
+		map.put("title", item.getTitle());
+		map.put("tracker", item.getTracker());
+		map.put("unread", item.isUnread());
+		return map;
 	}
 
 
@@ -612,6 +832,19 @@ RCMPlugin
 			plugin_interface.getUtilities().unregisterJSONRPCServer((Utilities.JSONServer)json_rpc_server);
 			
 			json_rpc_server = null;
+		}
+
+		if (mapSearchResults != null) {
+			mapSearchResults.clear();
+		}
+		if (mapSearchInstances != null) {
+  		for (SearchInstance si: mapSearchInstances.values()) {
+  			try {
+  				si.cancel();
+  			} catch (Throwable t) {
+  			}
+  		}
+  		mapSearchInstances.clear();
 		}
 	}
 	
